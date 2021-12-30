f8e28c235c7cf94da64c90378299eca7f21a6196
diegocedres <diegocedres@mbp-de-diego.home>
2021-12-30 15:37:34 +0000

    Kata5 version 2

---
+++ src/kata5p2/Kata5P2.java
@@ -0,0 +1,38 @@
+package kata5p2;
+
+import java.util.*;
+import model.*;
+import view.*;
+
+public class Kata5P2 {
+    
+    Histogram histogram;
+    List<Mail> mailList;
+
+    public static void main(String[] args) {
+        Kata5P2 kata4 = new Kata5P2();
+        kata4.execute();
+    }
+    
+    private void execute(){
+        input();
+        process();
+        output();
+    }
+    
+    private void input(){
+        MailListReaderBD m = new MailListReaderBD();
+        mailList = m.selectAll();
+    }
+    
+    private void process(){
+        MailHistogramBuilder m2 = new MailHistogramBuilder();
+        histogram = m2.build(mailList);
+    }
+    
+    private void output(){
+        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
+    }
+    
+}
---
+++ src/model/Mail.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Mail {
+    
+    private final String mail;
+    public Mail(String mail){
+        this.mail=mail;
+    }
+    
+    public String getDomains(){
+        Pattern pattern1 = Pattern.compile("@([a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*)");
+        Matcher matcher1 = pattern1.matcher(this.mail);
+  
+        matcher1.find();
+        return matcher1.group(1);
+    }
+}
